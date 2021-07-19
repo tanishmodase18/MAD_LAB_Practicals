d17030023f30e4f71616a682e123d938967f586f
tanishmodase18 <85211871+tanishmodase18@users.noreply.github.com>
2021-07-19 17:55:16 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.multithreading;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity
+{
+        Button bt1, bt2;
+        ImageView img;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState)
+        {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+
+                bt1 = (Button)findViewById(R.id.button1);
+                bt2 = (Button)findViewById(R.id.button2);
+                img = (ImageView)findViewById(R.id.imageView);
+
+                bt1.setOnClickListener(new View.OnClickListener()
+                {
+                        @Override
+                        public void onClick(View v)
+                        {
+                                new Thread(new Runnable()
+                                {
+                                        @Override
+                                        public void run()
+                                        {
+                                                img.post(new Runnable()
+                                                {
+                                                        @Override
+                                                        public void run()
+                                                        {
+                                                                img.setImageResource(R.drawable.wallpaper1);
+                                                        }
+                                                });
+                                        }
+                                }).start();
+                        }
+                });
+
+                bt2.setOnClickListener(new View.OnClickListener()
+                {
+                        @Override
+                        public void onClick(View v)
+                        {
+                                new Thread(new Runnable()
+                                {
+                                        @Override
+                                        public void run()
+                                        {
+                                                img.post(new Runnable()
+                                                {
+                                                        @Override
+                                                        public void run()
+                                                        {
+                                                                img.setImageResource(R.drawable.wallpaper2);
+                                                        }
+                                                });
+                                        }
+                                }).start();
+                        }
+                });
+
+        }
+}
